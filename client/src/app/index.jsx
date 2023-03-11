192e718397d3ab68571ed9b28bb04e765c5afb2f
eokrizz <crisvagnersd@gmail.com>
2023-03-11 19:30:40 -0300

    adiciona bootstrap e font-awesome

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -1,6 +1,9 @@
 import './styles.css'
 import React from 'react'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
 import Logo from '../components/Logo'
 import Nav from '../components/Nav'
 import Main from '../components/Main'
