f17345ed0a287ab6905eaf541c5a48cdfeeb674b
eokrizz <crisvagnersd@gmail.com>
2023-03-13 19:52:38 -0300

    adiciona conteúdo da home page

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Main from '../../components/main'
 
+const headerProps = {
+    icon: 'home',
+    title: 'Início',
+    subtitle: 'Aprendendo React'
+}
+
 export default function Home(props) {
     return (
-        <Main icon="home" title="Início"
-            subtitle="Projeto React">
-            Página Inicial
+        <Main {...headerProps}>
+            <div className='display-4'>Bem Vindo!</div>
+            <hr />
+            <p className="mb-0">Sistema para exemplificar a construção
+                de um cadastro desenvolvido em React!</p>
         </Main>
     )
 }
