d3b525fccfbbfffe11ac692fd6cfaa25d0ad9edb
eokrizz <crisvagnersd@gmail.com>
2023-03-13 15:35:06 -0300

    refatora os icons, trocando por componentes

--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -1,16 +1,14 @@
 import './styles.css'
 import React from 'react'
 
+import Icon from '../icons'
+
 export default function Nav(props) {
     return (
         <aside className="menu-area">
             <nav className="menu">
-                <a href="/">
-                    <i className="fa fa-home"></i> Início
-                </a>
-                <a href="/users">
-                    <i className="fa fa-users"></i> Usuários
-                </a>
+                <Icon href='/' icon='home' content='Início' />
+                <Icon href='/users' icon='users' content='Usuários' />
             </nav>
         </aside>
     )
